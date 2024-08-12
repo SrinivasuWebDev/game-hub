7f1e284730bb9603423f23bb16c232cbc9072889
srinivasukurapati <kurapatisrinivasu420@gmail.com>
2024-08-12 16:52:23 +0530

    Aside_Loader Is Created

--- src/component/GenereList.tsx
+++ src/component/GenereList.tsx
@@ -1,9 +1,14 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react"
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react"
 import UseGener from "../hooks/useGenere"
 import getCropedIamge from "../service/getCropedImage"
 
 const GenereList = () => {
-    const {data}=UseGener()
+    const {data,isloading,error}=UseGener()
+
+    if(error) return null;
+    
+    if(isloading) return <Spinner/>
+
   return (
     <List>{data.map((gener)=><ListItem paddingY={3}>
         <HStack>
