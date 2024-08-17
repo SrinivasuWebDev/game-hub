80e5bd863bc4fb3f7fe628a9163bd22ba6f75a79
srinivasukurapati <kurapatisrinivasu420@gmail.com>
2024-08-17 17:11:25 +0530

    genereList_heading created and cleanup

--- src/component/GenereList.tsx
+++ src/component/GenereList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react"
+import { Button, Heading, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react"
 import UseGener, { Gener } from "../hooks/useGenere"
 import getCropedIamge from "../service/getCropedImage"
 
@@ -15,13 +15,18 @@ const GenereList = ({onselectGenere,selectedGenere}:Props) => {
     
     if(isloading) return <Spinner/>
 
-  return (
+  return (<>
+    <Heading fontSize="2xl" marginY={1}>Genere</Heading>
     <List>{data.map((gener)=><ListItem key={gener.id} paddingY={3}>
         <HStack>
-           <Image borderRadius="8px" boxSize="36px" src={getCropedIamge(gener.image_background)}></Image>
-           <Button fontWeight={gener.id===selectedGenere?.id?"bold":"normal"}  variant="link" fontSize="lg" onClick={()=>{onselectGenere(gener)}}>{gener.name}</Button>
+           <Image borderRadius="8px" boxSize="36px" 
+              src={getCropedIamge(gener.image_background)}
+              objectFit="cover">  
+           </Image>
+           <Button whiteSpace="normal" textAlign="left" fontWeight={gener.id===selectedGenere?.id?"bold":"normal"}  variant="link" fontSize="lg" onClick={()=>{onselectGenere(gener)}}>{gener.name}</Button>
         </HStack>
     </ListItem>)}</List>
+    </>
   )
 }
 
